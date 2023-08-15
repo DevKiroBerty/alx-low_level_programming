60c61a0c2b6f3567672a0429d034942740df7e4b
DevKiroBerty <kirollos.rezk@gmail.com>
2023-08-15 16:28:13 -0700

    My second commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,15 @@
 #include "main.h"
-void print_alphabet(void) 
+/**
+  * print_alphabet - Make the alphabet
+  *
+  * Return: void
+  */
+void print_alphabet(void)
 {
-    char c;
-    for (c = 'a'; c <= 'z'; c++)
-    {
-	    _putchar('c');
-    }
-	_putchar('\n');    
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
 }
