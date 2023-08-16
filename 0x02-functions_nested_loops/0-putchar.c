792ce1333319f1a6c991e4268464223abd7a8741
DevKiroBerty <kirollos.rezk@gmail.com>
2023-08-15 18:51:36 -0700

    My tenth commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
 #include "main.h"
-int main (void) 
+
+/**
+  * print_to_98 - Prints all natural numbers from n to 98
+  * @n: The number to start printing from
+  *
+  * Return: Always 0.
+  */
+void main(void)
 {
-   _putchar('_');
-   _putchar('p');
-   _putchar('u');
-   _putchar('t');
-   _putchar('c');
-   _putchar('h');
-   _putchar('a');
-   _putchar('r');
-   _putchar('\n');
-   return(0);
+	_putchar(_);
+	_putchar(p);
+	_putchar(u);
+	_putchar(t);
+	_putchar(c);
+	_putchar(h);
+	_putchar(a);
+	_putchar(r);
+	_putchar('\n');
+	return (0);
 }
--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,9 +10,19 @@
 void print_to_98(int n)
 {
 	int i;
-	for (i = n; i<=98; i++)
+
+	if (n < 98)
 	{
-		_putchar(i);
+		for (i = n; i <= 98; i++)
+		{
+			_putchar(i);
+		}
 	}
-	_putchar('\n');
+	else if (n > 98)
+	{
+		for (i = n; i >= 98; i--)
+                {
+                        _putchar(i);
+                }
+	}
 }
