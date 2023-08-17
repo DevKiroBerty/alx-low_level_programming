605600516162f88d66bb7b6b4ad4af52d16153c1
DevKiroBerty <kirollos.rezk@gmail.com>
2023-08-17 11:45:40 -0700

    My tenth commit

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -7,19 +7,19 @@
 */
 void print_square(int size)
 {
-	int i;
-	int j;
+int i;
+int j;
 
-	if (size > 0)
-	{
-		for (i = 1; i <= size; i++)
-		{
-			for (j = 1; j <= size; j++)
-			{
-				_putchar('#');
-			}
-			_putchar('\n');
-		}
-	}
-	_putchar('\n');
+if (size > 0)
+{
+for (i = 1; i <= size; i++)
+{
+for (j = 1; j <= size; j++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+_putchar('\n');
 }
